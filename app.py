ba28e09ca40e42f9088b4a84b2f21df8d1a91708
Jeni Williams <stanfordjeni@gmail.com>
2024-12-29 18:16:36 -0800

    Defined the tables, schemas for the API project - error free

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# PROJECT LIBRARY IMPORT STATEMENTS
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -6,9 +7,11 @@ from marshmallow import ValidationError
 import os
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, Table, String, Column, select, DateTime
+from sqlalchemy import ForeignKey, Table, String, Column, select, DateTime, Float
 from typing import List
+from datetime import datetime
 
+# ===  API CONFIGURATION === #
 # INIT FLASK APP
 app = Flask(__name__)
 
@@ -16,7 +19,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:JWhod94612!@localhost/ecommerce_api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# === CREATE DATA BASE MODEL ===
+# === CREATE DATA BASE MODEL === #
 class Base(DeclarativeBase):
     pass
 
@@ -24,4 +27,82 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 ma = Marshmallow(app)
+
+## TABLE DEFINITIONS ##
+# => ORDER PRODUCT ASSOCIATION TABLE
+# ● order_id:Integer,foreign key referencing Order
+# ● product_id:Integer,foreign key referencing Product
+
+order_product = Table(
+    "order_product",
+    Base.metadata,
+    Column("order_id", ForeignKey("app_orders.id")),
+    Column("product_id", ForeignKey("app_products.id"))
+)
+
+# => USER TABLE
+# id:Integer, primary key, auto-increment
+# name: String
+# address: String
+# email: String (must be unique)
+
+class User(Base):
+    __tablename__ = "user_accounts"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    address: Mapped[str] = mapped_column(String(200), nullable=False)
+    email: Mapped[str] = mapped_column(String(200), unique=True)
+    
+    #One to Many Relationship: 1 User => List of Orders
+    orders: Mapped[List["Order"]] = relationship(back_populates="user")
+
+# => ORDER TABLE
+# ● id: Integer, primary key, auto-increment
+# ● order_date: DateTime (learn to use DateTime in SQLAlchemy)
+# ● user_id: Integer, foreign key referencing User
+
+class Order(Base):
+    __tablename__ = "app_orders"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    order_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_accounts.id"))
+    
+    # Many to One Relationship: List of Orders => 1 User
+    user: Mapped["User"] = relationship(back_populates="orders")
+
+# => PRODUCT TABLE
+# id:Integer, primary key, auto-increment
+# ● product_name: String
+# ● price: Float
+
+class Product(Base):
+    __tablename__ = "app_products"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    product_name: Mapped[str] = mapped_column(String(200), nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+
+# === MARSHMALLOW SCHEMA DEFINITIONS === #
+## SCHEMA DEFINITIONS ##
+# ● UserSchema
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+
+# ● OrderSchema
+class OrderSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Order
+        
+# ● ProductSchema
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+
+## INIT SCHEMAS ##
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+order_schema = OrderSchema()
+orders_schema = OrderSchema(many=True)
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
